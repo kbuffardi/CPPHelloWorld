5d47541a16e8ac4d5159f28347f56de09bce568c
Mohit Athwani <mohit@athwani.net>
2017-02-15 11:13:54 -0800

    Added simple console based app to print in Swift

--- README.md
+++
@@ -1,3 +0,0 @@
-# C++ Hello World
-
-This program illustrates a very simple C++ console application that displays the message "Hello, World" and then terminates."
---
+++ mohit_athwani_print.swift
@@ -0,0 +1,6 @@
+/**
+* Filename: mohit_athwani_print.swift
+* Author: Mohit Athwani
+* Description: Simple console application in Swift to print Hello, World!
+*/
+print("Hello, World!")
