38195f6e8af3b02a607044bb01a8f088e2b16036
tkhandke <tanutd.khandke@gmail.com>
2017-02-15 11:34:50 -0800

    edited main.c

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
+/*
+Author:Tanay Khandke
+file:main.c
+This program illustrates a very simple C console application that displays 
+the message "Hello, World" and then terminates."
+*/
 #include <stdio.h>
 
 int main()
